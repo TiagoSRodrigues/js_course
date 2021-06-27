feb69cf1fb1b7a5aed60fa5915cad7be0246e15a
Tiago Rodrigues <tiago4r@gmail.com>
2021-06-27 11:09:05 +0100

    class 1

---
+++ script.js
@@ -0,0 +1,13 @@
+let js = "Amazing";
+
+console.log(40+8+23-10);
+
+console.log("jonas");
+console.log(13);
+
+let firstname = "jonas";
+
+
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
